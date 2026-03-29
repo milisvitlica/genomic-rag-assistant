80f671b1b0cc2ea6893127fc3379c41fcffea694
Milijana Svitlica <221366344+milisvitlica@users.noreply.github.com>
2026-03-29 13:02:44 +0200

    Initial commit

---
+++ test_torch.py
@@ -0,0 +1,11 @@
+import torch
+import numpy
+
+# x= torch.tensor([1,2,3])
+# y = x*2
+
+# print("Tensor:",x)
+# print("Result:",y)
+
+print("Is CUDA Available:", torch.cuda.is_available())
+print("Torch Version:", torch.__version__)
